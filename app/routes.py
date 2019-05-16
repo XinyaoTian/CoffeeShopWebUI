5cc4889193219fe84b51da315a7fca24497c0f0d
XinyaoTian <leontian1024@gmail.com>
2019-05-16 18:56:53 +0800

    index.html tested completed

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Welcome to my CoffeeShop."
+    return render_template('index.html')
+
